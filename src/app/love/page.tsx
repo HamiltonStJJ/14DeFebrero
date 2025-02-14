bc51e0ce2dae70e8cfd790b80b5ecc94400372db
HamiltonStJJ <hamil.jumbo@gmail.com>
2025-02-13 22:51:37 -0500

    Edicion final Dos

--- src/app/love/page.tsx
+++ src/app/love/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { HeartHandshake, Sparkles } from 'lucide-react';
 import Image from 'next/image';
